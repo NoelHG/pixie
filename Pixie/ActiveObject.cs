549a3cffe20444173dc6bc42869b4be03aeb6f69
Noel H. Gonzalez <noelhego@gmail.com>
2020-12-07 22:14:17 +0900

    Add direction, primitive movement and layer organization

---
+++ Pixie/ActiveObject.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Represents an object that interacts with the Pixie framework */
+public class ActiveObject : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// If this is true, the game will automatically choose an offset such that the base of the sprite marks the spot where the layering should switch.
+    /// It would be like if the object's lowest Y position is also the bottom of the sprite, with a 0 value for spriteOffset.
+    /// This option is very useful if you're lazy, or sprites are centered on the objects. 
+    /// Overrides the inputted value for spriteOffset.
+    /// </summary>
+    [SerializeField] bool autoDetectOffSet;
+
+    /// <summary>
+    /// This is the difference between the object's Y position and the intended "bottom". 
+    /// 0 is a good value if the object's lowest Y position is also the bottom of the sprite.
+    /// </summary>
+    [SerializeField] int spriteOffset;
+
+
+    /// <summary>
+    /// Movement speed base. Used with the time step to calculate movement in MovementFrame()
+    /// </summary>
+    [SerializeField] int movementSpeed = 0;
+    //This is changed if using the sprint mechanism
+    int currentSpeed;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        if (autoDetectOffSet)
+        {
+            AutoOffset();
+        }
+
+        currentSpeed = movementSpeed;
+
+    }
+
+    void LateUpdate()
+    {
+        ComputeSortingOrder();
+    }
+
+    void ComputeSortingOrder()
+    {
+        spriteRenderer.sortingOrder = (int)(gameObject.transform.position.y - spriteOffset) * -1;
+    }
+
+    void AutoOffset()
+    {
+        spriteOffset = (int)(gameObject.transform.position.y - Camera.main.WorldToScreenPoint(spriteRenderer.bounds.min).y);
+    }
+
+
+    /// <summary>
+    /// Represents what the object will do in a movement timestep.
+    /// </summary>
+    /// <param name="direction">One of the four directions specified in the Direction class</param>
+    void MovementFrame(Direction direction)
+    {
+        transform.Translate(direction.MovementTimeStep(currentSpeed));
+    }
+}
---
+++ Pixie/Internal/Direction.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Directions for use in animations and collider checks.
+/// </summary>
+public class Direction
+{
+    readonly string value;
+    private Direction(string val) { value = val; }
+
+    public static Direction Up { get { return new Direction("Up"); } }
+    public static Direction Down { get { return new Direction("Down"); } }
+    public static Direction Left { get { return new Direction("Left"); } }
+    public static Direction Right { get { return new Direction("Right"); } }
+
+    /// <summary>
+    /// The direction name string, for animations.
+    /// </summary>
+    public override string ToString()
+    {
+        return this.value;
+    }
+
+    /// <summary>
+    /// The direction as a vector2. Used for collider checks.
+    /// </summary>
+    public Vector2 ToVector2()
+    {
+        switch (this.value) {
+            case "Up":
+                return new Vector2(0, 1);
+            case "Down":
+                return new Vector2(0, -1);
+            case "Left":
+                return new Vector2(-1, 0);
+            case "Right":
+                return new Vector2(1, 0);
+            default:
+                throw new System.ArgumentException("Invalid direction");
+        }
+    }
+
+    /// <summary>
+    /// The amount to move in a single time step.
+    /// </summary>
+    public Vector2 MovementTimeStep(int speed)
+    {
+        switch (this.value)
+        {
+            case "Up":
+                return new Vector2(0, speed * Time.deltaTime);
+            case "Down":
+                return new Vector2(0, -speed * Time.deltaTime);
+            case "Left":
+                return new Vector2(-speed * Time.deltaTime, 0);
+            case "Right":
+                return new Vector2(speed * Time.deltaTime, 0);
+            default:
+                throw new System.ArgumentException("Invalid direction");
+        }
+    }
+}
